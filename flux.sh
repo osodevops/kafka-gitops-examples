a95e54a0a745ef90bc032d0d0ce544fd8a459cc6
Andrew McCully <mccullya@gmail.com>
2021-05-12 17:01:56 +0100

    typo

--- flux.sh
+++ flux.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-1
-
-
 flux bootstrap github \
 --context=minikube \
 --owner=${GITHUB_USER} \
 --repository=kakfa-gitops \
 --path=clusters/dev
---branch=main \
+--branch=andrew \
 --personal \
