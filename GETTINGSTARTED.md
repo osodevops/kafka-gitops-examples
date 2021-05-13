f8c4ebd3b04ce17b170c217ef167a8667d5205af
Andrew McCully <mccullya@gmail.com>
2021-05-13 14:18:43 +0100

    debug

--- GETTINGSTARTED.md
+++ GETTINGSTARTED.md
@@ -7,7 +7,9 @@ export APIKEY=<API KEY>
 export EMAIL=<user email here>
 ```
 
-To deploy the operator, ensure 'kubectl' is pointing to the correct context, and run `$ ./install_operator.sh`.
+
+
+To deploy the operator, ensure 'kubectl' is pointing to the correct context, and run `$ source ./install_operator.sh`.
 
 To verify the operator has installed successfully, run kubectl `get pods -n confluent` where you should see:
 
@@ -15,19 +17,6 @@ To verify the operator has installed successfully, run kubectl `get pods -n conf
 NAMESPACE     NAME                                  READY   STATUS    RESTARTS   AGE
 confluent     confluent-operator-5b99cdd9d9-pcx2p   1/1     Running   0          3m44s
 ```
-
-
-#### Bootstrap this repository
-You will need SSH access to this repository
-
-
-flux bootstrap github \
---owner=mccullya \
---repository=kakfa-gitops \
---path=clusters/dev \
---branch=andrew
---personal
-
 
 
 #### Useful commands
--- install_operator.sh
+++ install_operator.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-kubectl create namespace confluent
-kubectl create namespace flux-system
-kubectl config set-context --current --namespace=confluent
-
-
-
-
-kubectl create secret -n confluent docker-registry confluent-registry \
-  --docker-server=confluent-docker-internal-early-access-operator-2.jfrog.io \
-  --docker-username=$USER \
-  --docker-password=$APIKEY \
-  --docker-email=$EMAIL
-kubectl create secret -n flux-system generic https-credentials \
-  --username=$USER \
-  --password=$APIKEY
 flux bootstrap github \
 --context=minikube \
 --owner=${GITHUB_USER} \
@@ -21,20 +6,12 @@ flux bootstrap github \
 --path=clusters/dev \
 --branch=andrew \
 --personal
-
-
-
-
-helm repo add confluentinc_earlyaccess \
-  https://confluent.jfrog.io/confluent/helm-early-access-operator-2 \
-  --username $USER \
-  --password $APIKEY
-helm repo update
-
-# AKA /infrastructure/confluent/confluent-operators.yaml
-#helm upgrade --install operator \
-#confluentinc_earlyaccess/confluent-for-kubernetes \
-#--set image.registry=confluent-docker-internal-early-access-operator-2.jfrog.io
-
-# AKA /infrastructure/tools/openldap.yaml
-#helm upgrade --install -f ./resources/openldap/ldaps-rbac.yaml test-ldap ./resources/openldap --namespace confluent
+sleep 100
+kubectl create secret -n confluent docker-registry confluent-registry \
+  --docker-server=confluent-docker-internal-early-access-operator-2.jfrog.io \
+  --docker-username=$USER \
+  --docker-password=$APIKEY \
+  --docker-email=$EMAIL && \
+kubectl create secret -n flux-system generic https-credentials \
+  --from-literal=username=$USER \
+  --from-literal=password=$APIKEY
