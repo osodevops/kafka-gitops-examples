74788d0085aeb9398a71e2c3091a406bc012da5c
Andrew McCully <mccullya@gmail.com>
2021-05-18 12:40:18 +0100

    rework of secrets, introducing multi-environment deployment

--- resources/populate_secrets.sh
+++ resources/populate_secrets.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-kubectl create secret -n confluent generic credential \
+kubectl create secret generic credential \
 --from-file=plain-users.json=./users/creds-kafka-sasl-users.json \
 --from-file=digest-users.json=./users/creds-zookeeper-sasl-digest-users.json \
 --from-file=digest.txt=./users/creds-kafka-zookeeper-credentials.txt \
@@ -7,28 +7,37 @@ kubectl create secret -n confluent generic credential \
 --from-file=basic.txt=./users/creds-control-center-users.txt \
 --from-file=ldap.txt=./users/ldap.txt \
 --from-file=mdsPublicKey.pem=./certs/mds-publickey.txt \
---from-file=mdsTokenKeyPair.pem=./certs/mds-tokenkeypair.txt
-kubectl create secret -n confluent generic mds-token \
+--from-file=mdsTokenKeyPair.pem=./certs/mds-tokenkeypair.txt \
+--dry-run=client --output=yaml > ../kustomize/base/secrets/credential.yaml
+
+kubectl create secret generic mds-token \
 --from-file=mdsPublicKey.pem=./certs/mds-publickey.txt \
---from-file=mdsTokenKeyPair.pem=./certs/mds-tokenkeypair.txt
+--from-file=mdsTokenKeyPair.pem=./certs/mds-tokenkeypair.txt \
+--dry-run=client --output=yaml > ../kustomize/base/secrets/mds-token.yaml
 
 # Kafka RBAC credential
-kubectl create secret -n confluent generic mds-client \
---from-file=bearer.txt=./users/bearer.txt
+kubectl create secret generic mds-client \
+--from-file=bearer.txt=./users/bearer.txt \
+--dry-run=client --output=yaml > ../kustomize/base/secrets/mds-client.yaml
 # Control Center RBAC credential
-kubectl create secret -n confluent generic c3-mds-client \
---from-file=bearer.txt=./users/c3-mds-client.txt
+kubectl create secret generic c3-mds-client \
+--from-file=bearer.txt=./users/c3-mds-client.txt \
+--dry-run=client --output=yaml > ../kustomize/base/secrets/c3-mds-client.yaml
 # Connect RBAC credential
-kubectl create secret -n confluent generic connect-mds-client \
---from-file=bearer.txt=./users/connect-mds-client.txt
+kubectl create secret generic connect-mds-client \
+--from-file=bearer.txt=./users/connect-mds-client.txt \
+--dry-run=client --output=yaml > ../kustomize/base/secrets/connect-mds-client.yaml
 # Schema Registry RBAC credential
-kubectl create secret -n confluent generic sr-mds-client \
---from-file=bearer.txt=./users/sr-mds-client.txt
+kubectl create secret generic sr-mds-client \
+--from-file=bearer.txt=./users/sr-mds-client.txt \
+--dry-run=client --output=yaml > ../kustomize/base/secrets/sr-mds-client.yaml
 # ksqlDB RBAC credential
-kubectl create secret -n confluent generic ksqldb-mds-client \
---from-file=bearer.txt=./users/ksqldb-mds-client.txt
+kubectl create secret generic ksqldb-mds-client \
+--from-file=bearer.txt=./users/ksqldb-mds-client.txt \
+--dry-run=client --output=yaml > ../kustomize/base/secrets/ksqldb-mds-client.yaml
 # Kafka REST credential
-kubectl create secret -n confluent generic rest-credential \
+kubectl create secret generic rest-credential \
 --from-file=bearer.txt=./users/bearer.txt \
 --from-file=basic.txt=./users/bearer.txt \
---from-file=plain.txt=./users/bearer.txt
+--from-file=plain.txt=./users/bearer.txt \
+--dry-run=client --output=yaml > ../kustomize/base/secrets/rest-credential.yaml
